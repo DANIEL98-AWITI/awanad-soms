07476327dd4826f50fa26739e0499bef18a47651
DANIEL98-AWITI <awitidaniel105@gmail.com>
2026-07-15 09:43:48 +0300

    AAdd sample vessel datadd sample vessel data to data.js

---
+++ src/src/src/data.js
@@ -0,0 +1,18 @@
+// Sample Vessel Data
+
+const vessels = [
+    {
+        vessel: "MSC POLARIS",
+        containers: 15,
+        evacuated: 15,
+        status: "Completed"
+    },
+    {
+        vessel: "XIN HUANG PU",
+        containers: 14,
+        evacuated: 2,
+        status: "In Progress"
+    }
+];
+
+console.log(vessels);
