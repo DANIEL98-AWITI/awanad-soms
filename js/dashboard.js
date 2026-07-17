6d5d47f860a3fda7bebb6801075dbd2d67211c2e
DANIEL98-AWITI <awitidaniel105@gmail.com>
2026-07-17 13:14:34 +0300

    Implement saveVessel function to add vessel details
    Added functionality to save vessel details to a table and reset form fields.

--- js/dashboard.js
+++ js/dashboard.js
@@ -29,8 +29,35 @@ document.getElementById("vesselForm").style.display="flex";
 
 function saveVessel(){
 
-alert("Vessel Registered Successfully!");
+let vessel=document.getElementById("vesselName").value;
+
+let line=document.getElementById("shippingLine").value;
+
+let eta=document.getElementById("eta").value;
+
+let ata=document.getElementById("ata").value;
+
+let cargo=document.getElementById("cargo").value;
+
+let table=document.getElementById("vesselTableBody");
+
+let row=table.insertRow();
+
+row.innerHTML=`
+<td>${vessel}</td>
+<td>${line}</td>
+<td>${eta}</td>
+<td>${ata}</td>
+<td>${cargo}</td>
+<td><span class="badge green">Active</span></td>
+<td>✏️ 🗑️</td>
+`;
 
 document.getElementById("vesselForm").style.display="none";
+
+document.getElementById("vesselName").value="";
+document.getElementById("shippingLine").value="";
+document.getElementById("eta").value="";
+document.getElementById("ata").value="";
 
 }
